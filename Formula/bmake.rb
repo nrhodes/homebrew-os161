00651defe460ab157869fae89876f0b40c7ddc7a
nrhodes <neil@pobox.com>
2013-12-29 08:01:57 -0800

    Update bmake.rb to latest version from harvard

--- Formula/bmake.rb
+++ Formula/bmake.rb
@@ -1,16 +1,16 @@
 require 'formula'
 
 class BmakeMK < Formula
-  url 'http://www.cse.unsw.edu.au/~cs3231/10s1/os161/files/mk.tar.gz'
+  url 'http://www.eecs.harvard.edu/~dholland/os161/download/mk-20100612.tar.gz'
   version '1'
-  md5 'c5d840ebe6e337e617bb756953dcad14'
+  md5 '333b2cb2eb7087fc874584b58522efe4'
   
 end
 
 class Bmake < Formula
   homepage ''
-  url 'http://www.cse.unsw.edu.au/~cs3231/10s1/os161/files/bmake.tar.gz'
-  md5 '5960a18d5bf01da17fc60378cb2bfc3b'
+  url 'http://www.eecs.harvard.edu/~dholland/os161/download/bmake-20101215.tar.gz'
+  md5 'a64e489368f0bc80de326832dcc436c5'
   version '1'
 
   def install
