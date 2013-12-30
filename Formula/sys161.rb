b98550b068c1350847fb92f492f8e3f3539b199b
nrhodes <neil@pobox.com>
2013-12-29 16:48:28 -0800

    sha255 to sha256

--- Formula/sys161.rb
+++ Formula/sys161.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Sys161 < Formula
   url 'http://www.eecs.harvard.edu/~dholland/os161/download/sys161-1.99.06.tar.gz'
   version '1.99.06'
-  sha255 '7cacbadbccb183ce4ca405e73e91d374e00e92fd0606b57617840b381fff37be'
+  sha256 '7cacbadbccb183ce4ca405e73e91d374e00e92fd0606b57617840b381fff37be'
   
   depends_on "os161-binutils"
   depends_on "os161-gcc"
