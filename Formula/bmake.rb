3b20dbb18fd9cf71d1545b7fc7321657647ecac8
nrhodes <neil@pobox.com>
2014-03-21 07:59:47 -0700

    Update bmake.rb

--- Formula/bmake.rb
+++ Formula/bmake.rb
@@ -1,9 +1,5 @@
 require 'formula'
 
-resource 'mk' do
-  url 'http://www.eecs.harvard.edu/~dholland/os161/download/mk-20100612.tar.gz'
-  sha256 '37abd0c420f9caec56af27909b8cdda7e81fd4c4eac6a15e4583511693050e7b'
-end
 
 class Bmake < Formula
   homepage ''
@@ -11,6 +7,11 @@ class Bmake < Formula
   sha256 'aea63d5de54540445e9cd3f4b6f603bdabaebabff8e0567ad6d420b8078cd11f'
   version '1'
 
+  resource 'mk' do
+    url 'http://www.eecs.harvard.edu/~dholland/os161/download/mk-20100612.tar.gz'
+    sha256 '37abd0c420f9caec56af27909b8cdda7e81fd4c4eac6a15e4583511693050e7b'
+  end
+  
   def install
     resource("mk").stage { System echo }
     system "./configure", "--prefix=#{prefix}"
